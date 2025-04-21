19063644d90e0f92384cac3a77adf246846ea441
akash0233 <akashkale0611@gmail.com>
2025-04-21 19:24:14 +0530

    changes made by akash

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello    amhkahjkagjkg");
         System.out.println("jsJKHJFHJFHJ");
+        System.out.println("65646+46546+4");
     }
 }
